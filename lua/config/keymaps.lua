89e5398c535ff785cbb89cb2e541c65529d2ebe4
Grant Sander <gksander93@gmail.com>
2023-09-16 22:07:55 -0500

    more tweaks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<leader><space>",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "Resume telescope" }
+)
+
+vim.keymap.set("n", "<leader>;", function()
+  local r = unpack(vim.api.nvim_win_get_cursor(0))
+  return require("mini.comment").toggle_lines(r, r)
+end)
